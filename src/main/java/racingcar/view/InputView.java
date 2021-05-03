fe300ebffc03b1e499ddd05e9d41df5c6131cb3c
pok3mon025 <pok3mon025@gmail.com>
2021-05-03 22:47:00 +0900

    feat : 시도 횟수 입력 기능 구현 - scanner.nextInt() 로 숫자 유효성 체크

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
 	private static final String INPUT_RACING_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_RACING_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String[] getRacingCarName() {
 		System.out.println(INPUT_RACING_CAR_NAME_MESSAGE);
 		return scanner.nextLine().split(",");
+	}
+
+	public static int getRacingCount() {
+		System.out.println(INPUT_RACING_COUNT_MESSAGE);
+		return scanner.nextInt();
 	}
 }
