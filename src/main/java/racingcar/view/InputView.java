ad0af82b13c7e4fbae789d4ea57ea2ffeae22666
pok3mon025 <pok3mon025@gmail.com>
2021-05-04 20:41:43 +0900

    fix : 경주 횟수 유효성 체크 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class InputView {
 	private static final String INPUT_RACING_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String INPUT_RACING_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+	private static final String INPUT_RACING_COUNT_ERROR_MESSAGE = "[ERROR] 회수는 1 이상만 입력 가능합니다.";
+	private static final int RACING_COUNT_MIN_VALUE = 1;
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String[] getRacingCarName() {
@@ -14,6 +16,13 @@ public class InputView {
 
 	public static int getRacingCount() {
 		System.out.println(INPUT_RACING_COUNT_MESSAGE);
-		return scanner.nextInt();
+		return validationRacingCount(scanner.nextInt());
+	}
+
+	private static int validationRacingCount(int nextInt) {
+		if (nextInt < RACING_COUNT_MIN_VALUE) {
+			throw new IllegalArgumentException(INPUT_RACING_COUNT_ERROR_MESSAGE);
+		}
+		return nextInt;
 	}
 }
